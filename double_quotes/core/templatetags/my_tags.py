4861543d49ad59595d32c505b4349df280dda362
irbiseh <hristenko.evgeniy@yandex.ru>
2022-07-08 17:13:49 +0300

    Добваил приложение core добавил my_tags

---
+++ double_quotes/core/templatetags/my_tags.py
@@ -0,0 +1,25 @@
+from django import template
+
+from quotes.models import Quote, Author, QuoteSource
+
+
+register = template.Library()
+
+
+@register.simple_tag
+def total_quotes():
+    total = Quote.objects.count()
+    return total
+
+
+@register.simple_tag
+def total_authors():
+    total = Author.objects.count()
+    return total
+
+
+@register.simple_tag
+def total_quote_sources():
+    total = QuoteSource.objects.count()
+    return total
+
