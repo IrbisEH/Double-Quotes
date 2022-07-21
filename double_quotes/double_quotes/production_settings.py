71bb251ca2c6837a26c1d70cc1aceee6e879b534
mac-irbis-eh <hristenko.evgeniy@yandex.ru>
2022-07-21 12:27:15 +0000

    .

--- double_quotes/double_quotes/production_settings.py
+++ double_quotes/double_quotes/production_settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-THUMBNAIL_DEBUG = True
+DEBUG = False
+THUMBNAIL_DEBUG = False
 
-ALLOWED_HOSTS = ['51.250.109.204', '127.0.0.1', 'localhost', 'doublequote.ddns.net']
+ALLOWED_HOSTS = ['130.193.53.154', '127.0.0.1', 'localhost', 'doublequote.ddns.net']
 
 
 # Application definition
@@ -81,22 +81,15 @@ WSGI_APPLICATION = 'double_quotes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
-#         'NAME': os.getenv('DB_NAME'),
-#         'USER': os.getenv('POSTGRES_USER'),
-#         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-#         'HOST': os.getenv('DB_HOST'),
-#         'PORT': os.getenv('DB_PORT')
-#         }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
+        }
 }
 
 
