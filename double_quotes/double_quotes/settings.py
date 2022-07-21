0f47a561ab322855e045700162dc4560442eb80c
Христенко Евгений <hristenko.evgeniy@yandex.ru>
2022-07-21 16:52:33 +0300

    /

--- double_quotes/double_quotes/settings.py
+++ double_quotes/double_quotes/settings.py
@@ -26,8 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-THUMBNAIL_DEBUG = True
+DEBUG = False
+THUMBNAIL_DEBUG = False
 
 ALLOWED_HOSTS = ['130.193.53.154', '127.0.0.1', 'localhost', 'doublequote.ddns.net']
 
