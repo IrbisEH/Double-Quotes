945846eb90a012e2a43c848052af50693859c4db
Христенко Евгений <hristenko.evgeniy@yandex.ru>
2022-07-26 14:58:11 +0300

    добавил файлы контейнера

--- double_quotes/double_quotes/settings.py
+++ double_quotes/double_quotes/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-!ez$=kzbw(*@y2-#c650j1f*_(5c$tb)w7yyq-83-b(iqi6jg('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
