3596778b4615af5e3ea4584abd599d3e733c475c
Христенко Евгений <hristenko.evgeniy@yandex.ru>
2022-07-27 17:59:24 +0300

    новая версия settings

--- double_quotes/double_quotes/wsgi.py
+++ double_quotes/double_quotes/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault(
     'DJANGO_SETTINGS_MODULE',
-    'double_quotes.production_settings'
+    'double_quotes.settings'
 )
 
 application = get_wsgi_application()
